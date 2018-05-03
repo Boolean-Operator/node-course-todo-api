fba937b17024ef20378dcb02f48e37a62a5d6646
Boolean-Operator <marktgraybill@gmail.com>
2018-05-03 15:06:29 -0400

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,46 @@
+const {MongoClient, ObjectID}  = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server...');
+
+  //deleteMany
+
+  // db.collection('Todos').deleteMany({text: 'Eat Lunch'}).then((results) => {
+  //   console.log(results);
+  // });
+
+  //deleteOne
+
+  // db.collection('Todos').deleteOne({text: 'Eat Lunch'}).then((results) => {
+  //   console.log(results);
+  // });
+
+  //findOneAndDelete
+
+  // db.collection('Todos').findOneAndDelete({
+  //   completed: false
+  // }).then((results) => {
+  //   console.log(results);
+  // });
+
+
+  // db.collection('Users').deleteMany({
+  //   name: 'Mark'
+  // }).then((results) => {
+  //   console.log(results);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new  ObjectID("5aeaf5e303178c0d90b5214f")
+  }).then((results) => {
+    console.log(JSON.stringify(results, undefined, 2);
+  });
+
+
+
+  // db.close();
+
+});
