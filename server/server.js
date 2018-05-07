d3e1db939d73940cbf062430e014546def6ba18f
Boolean-Operator <marktgraybill@gmail.com>
2018-05-07 15:56:57 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,7 +22,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
-// app.get(()=> {});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+
 
 
 app.listen(3000, () => {
